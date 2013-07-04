610c2a534cb805d43d5ce91aa1c03db2f847dd2f
Gilberto Villa <ingilniero@gmail.com>
2013-07-03 21:14:42 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CucumberBddHowTo::Application.config.secret_key_base = '78f8e6c627ca378e571ad97cd642eaec93e50ec93ed7fef76e0f33676c60e388c0c61491e384b84f76537bedcbeb0944429b4354e61a0a4c26cc1b68b648279d'
